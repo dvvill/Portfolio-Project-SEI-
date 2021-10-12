b5af0bcd055a9fe490f81aad31c25032fada350b
dvvill <91037458+dvvill@users.noreply.github.com>
2021-10-12 18:09:02 -0400

    html, css and js file initiated

---
+++ app.js
@@ -0,0 +1,15 @@
+cost navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+// Toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+    // Animate Links
+    nav.navLinks.forEach((link, index) => {
+        console.log(index);
+    });
+}
+
+navSlide();
