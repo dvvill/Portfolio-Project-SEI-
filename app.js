0153fdabcdb31d714eff33eeaff51e643a243a41
dvvill <91037458+dvvill@users.noreply.github.com>
2021-10-13 04:16:28 -0400

    Hamburger navigation bar

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-cost navSlide = () => {
+const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-// Toggle Nav
+    // const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
+// // Toggle Nav
     burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+        document.querySelector('.nav-links').classList.toggle('.nav-active');
+        // nav.classList.toggle('.nav-active');
     });
-    // Animate Links
-    nav.navLinks.forEach((link, index) => {
-        console.log(index);
-    });
+//     // Animate Links
+//     nav.navLinks.forEach((link, index) => {
+//         console.log(index);
+//     });
 }
 
 navSlide();
+
