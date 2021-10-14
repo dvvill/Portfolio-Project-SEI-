349c971990c405f57a84b04bfb3bc8101bff3435
dvvill <91037458+dvvill@users.noreply.github.com>
2021-10-14 06:35:53 -0400

    skills and contact section complete

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ function myFunction() {
     }
   }
 
+
+//   function myFunction() {
+//   document.getElementById("projects").style.display = "block";
+// } 
+// 
+// function myFunction() {
+//   var x = document.getElementById("projects");
+//   if (window.matchMedia("(max-width: 700px)").matches) {
+    // x.innerHTML = "The screen is less than, or equal to, 700 pixels wide.";
+//   } else {
+    // x.innerHTML = "The screen is at least 700 pixels wide.";
+
