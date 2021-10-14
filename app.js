eaa198718ae60c2e324240db80a86942d5dd8ff5
dvvill <91037458+dvvill@users.noreply.github.com>
2021-10-13 21:23:11 -0400

    completed responsive navbar

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    // const nav = document.querySelector('.nav-links');
-//     const navLinks = document.querySelectorAll('.nav-links li');
-// // Toggle Nav
-    burger.addEventListener('click', () => {
-        document.querySelector('.nav-links').classList.toggle('.nav-active');
-        // nav.classList.toggle('.nav-active');
-    });
-//     // Animate Links
-//     nav.navLinks.forEach((link, index) => {
-//         console.log(index);
-//     });
-}
-
-navSlide();
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
